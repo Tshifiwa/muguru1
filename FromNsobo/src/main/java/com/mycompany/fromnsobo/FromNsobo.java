f40e6c2bfb12de32551f148d0fffed981c8a2bcc
Tshifiwa <163343330+Tshifiwa@users.noreply.github.com>
2024-07-31 16:49:48 +0200

    Add files via upload

---
+++ FromNsobo/src/main/java/com/mycompany/fromnsobo/FromNsobo.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.fromnsobo;
+
+public class FromNsobo {
+
+    public static void main(String[] args){
+    
+    for(int i=0;i<100;i++)
+        
+    {
+        System.out.println("I love you Tshifhiwa Muguru");
+    }
+}
+}
